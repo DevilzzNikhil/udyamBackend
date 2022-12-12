d0cbac3408e36565e3590eec1aefc6056108a9b6
Nikhil <nikhil10july123@gmail.com>
2022-12-12 23:41:34 +0530

    flake implemented

--- customauth/models.py
+++ customauth/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.core.validators import RegexValidator
 import re
 
 
@@ -17,21 +16,22 @@ YEARS = (
 )
 
 class AccountManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        print(email,password)
+
+    def create_user(self, email, password='Random'):
+        print(email, password)
         if not email:
             raise ValueError("The Email must be set")
-        
+
         email = self.normalize_email(email)
-        user = self.model(email = email)
+        user = self.model(email=email)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self,email,password):
+    def create_superuser(self, email, password):
         user = self.create_user(
             password=password,
-            email = self.normalize_email(email)
+            email=self.normalize_email(email)
         )
         user.is_admin = True
         user.is_staff = True
@@ -41,7 +41,7 @@ class AccountManager(BaseUserManager):
         return user
 
 class UserAcount(AbstractBaseUser):
-    email = models.EmailField( verbose_name="email", max_length=60, unique=True)
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
     date_joined = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
@@ -49,7 +49,7 @@ class UserAcount(AbstractBaseUser):
     name = models.CharField(max_length=100)
     college_name = models.CharField(max_length=200, blank=False, null=False)
     year = models.CharField(max_length=20, choices=YEARS, blank=False, null=False)
-    phone_number = models.CharField(validators=[isValid], max_length=15, blank=True,null=True)
+    phone_number = models.CharField(validators=[isValid], max_length=16, blank=False, null=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -58,15 +58,9 @@ class UserAcount(AbstractBaseUser):
 
     def __str__(self):
         return f"{self.email}"
-    
+
     def has_perm(self, perm, obj=None):
         return self.is_admin
 
     def has_module_perms(self, app_label):
         return True
-
-
-
-
-
-
